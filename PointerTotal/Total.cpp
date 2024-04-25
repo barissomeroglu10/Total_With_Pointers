2e96e5d855d6228e0f0f12d50fa74694efae9488
baris <baris@BARIS_SOMEROGLU>
2024-04-25 19:04:26 +0300

    Add project files.

---
+++ PointerTotal/Total.cpp
@@ -0,0 +1,44 @@
+﻿/*
+In this code, we will calculate sum for two numbers with using pointers and function.
+
+Developer = Barış Someroğlu
+Date = 25.04.2024 - 07:00 pm
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int TotalWithPointer(int*, int*);
+
+int main()
+{
+	int Number1, Number2;
+
+	cout << "Please Enter 1. Number = ";
+	cin >> Number1;
+
+	cout << "Please Enter 2. Number = ";
+	cin >> Number2;
+
+	// Pointer using
+	int* ptrNumber1 = &Number1;
+	int* ptrNumber2 = &Number2;
+
+	cout << endl;
+
+	cout << "Total of the numbers = " << TotalWithPointer(ptrNumber1, ptrNumber2) << endl << endl; // Call function 
+
+	return 0;
+}
+
+// Function definition
+int TotalWithPointer(int* ptrNumber1, int* ptrNumber2)
+{
+	int Total = 0;
+
+	Total = *ptrNumber1 + *ptrNumber2;
+
+	return Total;
+}
